e1eaa6676b06570b7fcf4de438af38a7a62f280f
Sudipto Ghosh <sudipto@ghosh.pro>
2019-05-26 20:06:15 +0530

    Understood Basic Layout

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LearningCSharp
 {
@@ -10,12 +6,7 @@ namespace LearningCSharp
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
